e401c636e002a2b40f11282e55867be48199a8e6
Paul Jones <shmiko@gmail.com>
2015-03-19 22:42:58 +1100

    more tidy

--- public/app/app.js
+++ public/app/app.js
@@ -2,12 +2,4 @@ angular.module('app', ['ngResource']);
 
 angular.module('app').controller('testCtrl', function($scope, $resource){
    $scope.jobs = $resource('/api/jobs').query();
-   //[{
-       //title: 'Sales Person',
-       //description: 'You will fight dragons'
-   //},{
-   //     title: 'Accountant',
-   //     description: 'you will use the keyboard'
-   //}];
-
 });
