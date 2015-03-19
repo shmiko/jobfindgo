768031484459177b15c2506cc4cdcab32d9c7c2f
Paul Jones <shmiko@gmail.com>
2015-03-20 07:39:19 +1100

    added mocha before and 2 more tests

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -16,14 +16,31 @@ function findJobs(query){
 }
 
 describe("get jobs", function(){
-	it("should never be empty since jobs are seeded", function(done){
+
+	var jobs;
+
+	before(function(done){
 		connectDB('mongodb://localhost/jobfinder')
 			.then(resetJobs)
 			.then(jobModel.seedJobs)
 			.then(findJobs)
-			.then(function(jobsList){
-				expect(jobsList.length).to.be.at.least(1);
+			.then(function(collection){
+				jobs = collection;
 				done();
 			});
+		});
+
+	it("should never be empty since jobs are seeded", function(){
+		expect(jobs.length).to.be.at.least(1);
 	});
+
+	it("should have a job with a title", function(){
+		expect(jobs[0].title).to.not.be.empty;
+	});
+
+	it("should have a job with a description", function(){
+		expect(jobs[0].description).to.not.be.empty;
+	});
+
+
 });
