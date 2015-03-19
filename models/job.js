ffa3866ad2abb65de8c3b61b2583f6ce70c95a0a
Paul Jones <shmiko@gmail.com>
2015-03-20 07:15:39 +1100

    added promises

--- models/job.js
+++ models/job.js
@@ -6,17 +6,27 @@ var jobSchema = mongoose.Schema({
     description:{type: String}
 });
 
+var jobs = [
+    {title:'Cook',description:'You will be making bagels'},
+    {title:'Waiter',description:'You will be putting food on peoples tables'},
+    {title:'Programmer',description:'You will be mindlessy typing'},
+    {title:'Axe Maker',description:'We need many axe\'s made..so many'}
+]
+
 var Job = mongoose.model('Job', jobSchema);
 
+function findJobs(query){
+    return Promise.cast(mongoose.model('Job').find(query).exec());
+}
+
+var createJob = Promise.promisify(Job.create, Job);
+
 exports.seedJobs = function(){
-    return new Promise(function(resolve,reject){
-        Job.find({}).exec(function(error, collection){
+    return findJobs({}).then(function(collection){
             if (collection.length === 0) {
-                Job.create({title:'Cook',description:'You will be making bagels'});
-                Job.create({title:'Waiter',description:'You will be putting food on peoples tables'});
-                Job.create({title:'Programmer',description:'You will be mindlessy typing'});
-                Job.create({title:'Axe Maker',description:'We need many axe\'s made..so many'},resolve);
+                return Promise.map(jobs,function(job){
+                    return createJob(job);
+                })
             }
-        });
-    });
+    })
 }
