1044b671a734a88d12230f7121cb2919b17c2440
Paul Jones <shmiko@gmail.com>
2015-09-20 17:13:33 +1000

    angeor.

--- angeor/socially/app.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient) {
-  angular.module('socially',['angular-meteor']);
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
